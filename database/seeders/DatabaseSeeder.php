385ac2ceb1ca52d9badf7e6ed591ac6239f8e1d0
kamranqk <98551290+kamranqk@users.noreply.github.com>
2022-08-06 20:19:34 -0400

    Add files via upload

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Education;
+use App\Models\User;
+use App\Models\Type;
+use App\Models\Project;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Education::truncate();
+        User::truncate();
+        Type::truncate();
+        Project::truncate();
+        
+        Education::factory()->count(2)->create();
+        User::factory()->count(2)->create();
+        Type::factory()->count(3)->create();
+        Project::factory()->count(4)->create();
+            
+    }
+}
